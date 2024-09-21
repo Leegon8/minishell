a041ce23b71831e01a351eb310e9540b1ecacab7
lprieto- <lprieto-@lprieto-U22.04vm>
2024-09-21 18:36:51 +0200

    tokenizer implementation

--- src/minishell.c
+++ src/minishell.c
@@ -6,15 +6,49 @@
 /*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 09:25:04 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/09/21 12:03:25 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/09/21 18:19:48 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envs)
+void	shell_loop(t_env *env, t_mshll *msh)
 {
 	char	*input;
+	char	*token;
+
+	while (msh->end_sig == 0)
+	{
+		rl_attempted_completion_function = command_completion;
+		input = readline("\033[1;96m👽 Space 🛸 shell $\e[0m> ");
+		if (input == NULL)
+			break ;
+		if (*input)
+			add_history(input);
+		
+		token = ft_strtok(input, " \t");
+		while (token != NULL)
+		{
+			if (strcmp("pwd", token) == 0) /* pwd */
+				printf("%s\n", env->pwd);
+			if (ft_strcmp("clearh", token) == 0) /* borra el historial */
+				clear_history();
+			if (ft_strcmp("clear", token) == 0) /* clear :D */
+				printf("%s", CLEAR);
+			// if (ft_strcmp("echo", token) == 0)
+			// 	ft_echo(argv);
+			if (ft_strcmp("exit", token) == 0) /* para hacer exit sin ctrl+C */
+				msh->end_sig = 1;
+			token = ft_strtok(NULL, " \t");
+		}
+	//	if (ft_strncmp("ls", input, 2))     /* execve rompe el bucle, hace exit */
+	//		execve("/bin/ls", argv, NULL);
+	free(input);
+	}
+}
+
+int	main(int argc, char **argv, char **envs)
+{
 	t_env	*env;
 	t_mshll	msh;
 
@@ -30,39 +64,11 @@ int	main(int argc, char **argv, char **envs)
 	env->pwd = malloc(PATH_MAX);
 	if (!env->pwd)
 		return (-1);
-	printf("pwd 1: %s\n", env->pwd);
-	printf("Numero de variables en env: %d\n", env_var_count(envs));
-	getcwd(env->pwd,PATH_MAX);
-	printf("pwd getcwd: %s\n", env->pwd);
-	// env->pwd = getenv("PWD");
-	// printf("pwd getenv: %s\n", env->pwd);
-	// {
-	// 	perror("Error: getcwd failed");
-	// 	return (-1);
-	// }
-	//init_env(env, envs);
+	printf("#N env vars: %d\n", env_var_count(envs));
+	init_env(env, envs);
+	shell_loop(env, &msh); /* Este es el loop principal, que esta en la funcion shell_loop */
 
-	while (msh.end_sig == 0)
-	{
-		rl_attempted_completion_function = command_completion;
-		input = readline("\033[1;96m👽 Space 🛸 shell $\e[0m> ");
-		if (input == NULL)
-			break ;
-		if (*input)
-			add_history(input);
-		if (ft_strcmp("pwd", input) == 0) /* pwd */
-			printf("%s\n", env->pwd);
-		if (ft_strncmp("clearh", input, 6) == 0) /* borra el historial */
-			clear_history();
-		if (ft_strncmp("clear", input, 5) == 0) /* clear :D */
-			printf("%s", CLEAR);
-		if (ft_strncmp("echo", input, 4) == 0)
-			ft_echo(argv);
-		if (ft_strncmp("exit", input, 4) == 0) /* para hacer exit sin ctrl+C */
-			msh.end_sig = 1;
-	//	if (ft_strncmp("ls", input, 2))     /* execve rompe el bucle, hace exit */
-	//		execve("/bin/ls", argv, NULL);
-		free(input);
-	}
+	free(env);
+	free(env->pwd);
 	return (0);
 }
--- src/ms_tokenizer.c
+++ src/ms_tokenizer.c
@@ -3,29 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ms_tokenizer.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lprieto- <lprieto-@student.42barcelon      +#+  +:+       +#+        */
+/*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 15:35:57 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/09/13 16:01:44 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/09/21 18:31:07 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* corta el str de entrada con un separador (modificable) No trabajo en ella */
+/* corta el str de entrada con un separador (no modificar) estoy trabajando en ella */
 char	*ft_strtok(char *str, const char *separator)
 {
-	char	*ptr;
-	char	*start;
+	static char	*ptr;
+	char		*start;
 
-	ptr = NULL;
-	start = NULL;
-	if (str == NULL)
+	if (str != NULL)
+		ptr = str;	
+	if(ptr == NULL)
+		return (NULL);
+	while (*ptr != '\0' && ft_strchr(separator, *ptr) != NULL)
+		ptr++;
+	if (*ptr == '\0')
 		return (NULL);
 	start = ptr;
 	while (*ptr != '\0' && ft_strchr(separator, *ptr) == NULL)
 		ptr++;
 	if (*ptr != '\0')
-		*ptr++ = '\0';
+	{
+		*ptr = '\0';
+		ptr++;
+	}
 	return (start);
 }
