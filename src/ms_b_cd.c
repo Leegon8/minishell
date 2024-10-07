e3d0c7e2e41adf5e79a867a770a5f37359f5c9ee
Leegon8 <legora011@gmail.com>
2024-10-07 17:44:45 +0200

    | > CD

--- src/ms_b_cd.c
+++ src/ms_b_cd.c
@@ -6,7 +6,7 @@
 /*   By: leegon <leegon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 14:41:10 by lauriago          #+#    #+#             */
-/*   Updated: 2024/10/07 16:41:27 by leegon           ###   ########.fr       */
+/*   Updated: 2024/10/07 17:43:43 by leegon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,20 @@
 /* Imprime los argumentos de echo, opción -n para suprimir el \n final */
 void	ft_cd(t_msh *msh, int num_cmd)
 {
+	DIR	*new_path;
+	int	i;
+	
+	i = 1;
+	new_path = NULL;
 	if (num_cmd > 2)
 		ft_fd_printf(2, "bash: %s: too many arguments\n", msh->tkns->cmd);
-	/*if (num_cmd == 2)
+	else if (num_cmd == 1)
+		chdir(msh->env->home);
+	/*else if (num_cmd == 2)
 	{
-		msh->env
+		printf("%d, Entra en cd function\n", num_cmd);
+		new_path = opendir(msh->tkns[i].cmd);
+		readdir(new_path);
+		ft_pwd(msh);
 	}*/
-	printf("%d, Entra en cd function\n", num_cmd);
 }
